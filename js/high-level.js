db0771491dafdd81e06127e460de56e349b6172f
Sanchya Mahajan <sanchya.mahajan@gmail.com>
2017-05-23 12:08:15 -0700

    Kanon and Sanchya started visualization

---
+++ js/high-level.js
@@ -0,0 +1,16 @@
+function HighLevel(){
+	var width = 960,
+        height = 500,
+    margin: {
+        top: 10,
+        right: 10,
+        bottom: 10,
+        left: 10
+        };
+    function chart(selection){
+    	selection.each(function(data) {
+    		
+    	}
+
+    }
+}
